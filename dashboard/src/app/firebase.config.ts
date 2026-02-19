3debbc59e8a6c4cc9eb8351f5c86fb503d093edd
Mxo01 <mariodimodica.01@gmail.com>
2026-02-19 20:13:39 +0100

    Add firebase to both projects and create firebase.config.ts

---
+++ dashboard/src/app/firebase.config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: 'AIzaSyB3xg_Qsa6fkWLqzi0oDV7Cz-3Qux4aZNQ',
+  authDomain: 'insieme-per-crescere-f477b.firebaseapp.com',
+  projectId: 'insieme-per-crescere-f477b',
+  storageBucket: 'insieme-per-crescere-f477b.firebasestorage.app',
+  messagingSenderId: '527365878949',
+  appId: '1:527365878949:web:0520a665a71df9da16094c',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
